7016f9db84073eebe68db46340de053e49986d5e
Demien Li <demien@appannie.com>
2015-07-01 17:05:55 +0800

    add engine

--- Hades/engine.py
+++ Hades/engine.py
@@ -110,9 +110,54 @@ def scrape_jd():
     }
     return start_hades(url, config)
 
+def test_p2p():
+    url = 'https://www.wanglibao.com/p2p/list'
+    config = {
+        "collection": {
+            "property_1": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-title-bar'] > a",
+                "re": ".*",
+                "name": "name",
+            },
+            "property_2": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-content'] > DIV[class='row'] > DIV:nth-child(1) > P > EM",
+                "re": ".*",
+                "name": "total",
+            },
+            "property_3": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-content'] > DIV[class='row'] > DIV:nth-child(2) > P > EM",
+                "re": ".*",
+                "name": "month",
+            },
+            "property_4": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-content'] > DIV[class='row'] > DIV:nth-child(3) > P > EM:nth-child(1)",
+                "re": ".*",
+                "name": "profit",
+            },
+            "property_5": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-content'] > DIV[class='row'] > DIV:nth-child(4) > P > EM",
+                "re": ".*",
+                "name": "avaiable",
+            },
+            "property_6": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-p2p--footer'] > DIV[class='row'] > DIV[class='span4'] > SPAN[class='highlight']",
+                "re": ".*",
+                "name": "process",
+            },
+            "property_7": {
+                "css": "HTML > BODY[class='white-background'] > DIV[class='container'] > DIV[class='row'] > DIV[class='span8'] > DIV[class='panel-p2p-product'] > DIV[class='shadow-inner'] > DIV[class='panel-content'] > DIV[class='row'] > DIV:nth-child(5) > P:nth-child(2)",
+                "re": ".*",
+                "name": "time_left",
+            }
+        }
+    }
+    return start_hades(url, config)
+
+
 if __name__ == '__main__':
-    print '*' * 20 + 'dian ping' + '*' * 20
-    print scrape_dianping()
-    print '*' * 20 + 'jd' + '*' * 20
-    print scrape_jd()
+    # print '*' * 20 + 'dian ping' + '*' * 20
+    # print scrape_dianping()
+    # print '*' * 20 + 'jd' + '*' * 20
+    # print scrape_jd()
+    print test_p2p()
     
